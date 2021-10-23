60083aa03409cf18b8f8bdfe003dac2c9373b844
B Bergfeld <b.bergfeld@hotmail.com>
2021-10-23 21:22:27 +0200

    Aligns rotation of heightmap display with heightmap as saved to file.

--- ProceduralCityBuilder/Application.cpp
+++ ProceduralCityBuilder/Application.cpp
@@ -48,17 +48,21 @@ void pcb::Application::mouseCallback(int button, int state, int x, int y) {
 
 pcb::Application::Application() : translationX(0), translationY(0), rotationZ(0), scale(1), mouseWindowX(0), mouseWindowY(0), globalRotationX(0), globalRotationY(0),
 isWarpingPointer(false), zoom(0),
-quadVertices(std::make_shared<std::vector<GLfloat>>(std::initializer_list<GLfloat>{
+quadVertices(std::make_shared<std::vector<GLfloat>>(std::initializer_list<GLfloat> {
 	-0.25f, -0.25f, 0.0f,
 	0.0f, -0.25f, 0.0f,
 	0.0f, 0.0f, 0.0f,
 	-0.25f, 0.0f, 0.0f
 })),
-quadTextureCoordinates(std::make_shared<std::vector<GLfloat>>(std::initializer_list<GLfloat>{
+quadTextureCoordinates(std::make_shared<std::vector<GLfloat>>(std::initializer_list<GLfloat> {
 	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+	0.0, 1.0
+	/*0.0, 0.0,
 	0.0, 1.0,
 	1.0, 1.0,
-	1.0, 0.0
+	1.0, 0.0*/
 })),
 heightmapTexture(nullptr), generatedHeightmapTexture(nullptr), renderObjects{ nullptr, nullptr, nullptr },
 terrainLayerRenderObjects(), shaderManager(), projectionMatrix() {}
